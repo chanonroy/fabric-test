0a341a1792bcf509b297ddae856163c3abf2a148
chanonroy <chanonroy@gmail.com>
2017-09-25 20:59:13 -0400

    changes

--- dist/main.js
+++ dist/main.js
@@ -61571,7 +61571,7 @@ var app = new _vue2.default({
         var payload = {
           'rack_size': this.frame_val ? this.pk_dict.frame[this.frame_val] : '', // { Number }
           'mesh': this.mesh_val ? this.pk_dict.mesh[this.mesh_val] : '', // { Number }
-          'logo_shape': this.badge ? this.pk_dict.badge[this.badge_shape] : '', // { String }
+          'logo_shape': this.badge ? this.pk_dict.badge[this.badge_val] : '', // { String }
           'logo': this.badge ? this.badge.toSVG() : '', // { String ? } <-- this is a giant SVG string
           'top': this.badge ? this.badge.top : '', // { Float }
           'left': this.badge ? this.badge.left : '', // { Float }
